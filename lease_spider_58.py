b3db3e7f4979d067fa6ca16b37456c2021090a24
Emmamama <mayixuan0302@gmail.com>
2016-11-14 13:31:55 -0800

    lease_spider_58
    区域、地段、面积、租金获取

--- lease_spider_58.py
+++ lease_spider_58.py
@@ -21,23 +21,25 @@ def lease_spider(max_pages):
 def get_single_item_data(item_url):
     source_code = requests.get(item_url)
     plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
+    #plain_text = plain_text.replace(' ','_')    #将空格转化为下划线
+    #plain_text = plain_text.replace('\n ','_')    #将空格转化为下划线
+    soup = BeautifulSoup(plain_text) 
     for item_name in soup.findAll('h1', {'style':'font-size:22px;'}):
         print (item_name.string)
     for item_info in soup.findAll('ul', {'class':'info'}):
-        item_info_text = item_info.get_text()
-        item_general_location = re.search(u"区域：.*",item_info_text).group()
-        item_specific_location = re.search(u"地段：.*",item_info_text).group()
-        item_area = re.search(u"面积：.*",item_info_text).group()
-        item_lease_fee = re.search(u"租金：.*",item_info_text).group()
+        item_info_text = item_info.get_text() 
+        item_general_location = re.search(u"区域：.*",item_info_text).group() #获取区域
+        item_specific_location = re.search(u"地段：.*",item_info_text).group() #获取地段
+        item_area = re.search(u"面积：.*",item_info_text).group() #获取面积
+        x = re.search(u"租金：.*",item_info_text,re.S) #租金匹配 
+        item_lease_fee = x.group() #获取租金
         print item_general_location
         print item_specific_location
         print item_area
-    #for item_lease_fee in soup.findAll('em', {'class':'redfont'}):
-        #print u"租金：" + (item_lease_fee.string)
-        #print item_info_text
+   # for item_lease_fee in soup.findAll('em', {'class':'redfont'}):
+       # print u"租金：" + (item_lease_fee.string)
+       # print item_info_text
         print item_lease_fee
 #保存至文件夹
-
 
 lease_spider(1)
